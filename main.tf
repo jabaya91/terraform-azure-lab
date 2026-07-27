1c3aaf74bed14f81d59145cdcb9533887dc48ec3
jabaya91 <97844947+jabaya91@users.noreply.github.com>
2026-07-27 00:51:18 -0500

    new test with vnets

--- main.tf
+++ main.tf
@@ -2,3 +2,58 @@ resource "azurerm_resource_group" "lab" {
   name     = "rg-tf-lab"
   location = "eastus"
 }
+resource "azurerm_resource_group" "lab" {
+  name     = "rg-tf-lab"
+  location = "eastus"
+}
+
+resource "azurerm_virtual_network" "lab" {
+  name                = "vnet-tf-lab"
+  address_space       = ["10.10.0.0/16"]
+  location            = azurerm_resource_group.lab.location
+  resource_group_name = azurerm_resource_group.lab.name
+}
+
+resource "azurerm_subnet" "lab" {
+  name                 = "subnet-workload"
+  resource_group_name  = azurerm_resource_group.lab.name
+  virtual_network_name = azurerm_virtual_network.lab.name
+  address_prefixes     = ["10.10.1.0/24"]
+}
+
+resource "azurerm_network_security_group" "lab" {
+  name                = "nsg-tf-lab"
+  location            = azurerm_resource_group.lab.location
+  resource_group_name = azurerm_resource_group.lab.name
+
+  security_rule {
+    name                       = "AllowHTTPS"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "443"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_subnet_network_security_group_association" "lab" {
+  subnet_id                 = azurerm_subnet.lab.id
+  network_security_group_id = azurerm_network_security_group.lab.id
+}
+
+resource "random_string" "suffix" {
+  length  = 6
+  special = false
+  upper   = false
+}
+
+resource "azurerm_storage_account" "lab" {
+  name                     = "st${random_string.suffix.result}"
+  resource_group_name      = azurerm_resource_group.lab.name
+  location                 = azurerm_resource_group.lab.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
--- providers.tf
+++ providers.tf
@@ -1,8 +1,15 @@
 terraform {
+  required_version = ">= 1.5"
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "~> 4.0"
+    }
+
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.6"
     }
   }
 }
